27e88c5749105d20748849b22ce59b93d936fa18
Mathew James Dagle <mathewdagle@gmail.com>
2024-05-28 22:37:06 +0800

    Third Commit

---
+++ src/app/rating/rating.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ratings',
+  standalone: true,
+  templateUrl: './rating.component.html',
+  styleUrl: './rating.component.less'
+})
+export class RatingsComponent {
+
+}
